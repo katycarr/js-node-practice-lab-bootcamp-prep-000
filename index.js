f5fe5f92909e1855b3b62d23fa834226b4c90234
Katy Carr <khcarr9@gmail.com>
2017-10-17 00:47:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Kraken"
-var height = "5"
+var height = 5
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height+1)
